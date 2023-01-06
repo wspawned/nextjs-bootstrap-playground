f65fab1836c7b96e9d05224100ce6ad6b29b091a
wspawned <wspawned@gmail.com>
2023-01-07 02:33:09 +0300

    Bootstrap install

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import '../styles/globals.css'
 
-export default function App({ Component, pageProps }) {
+import { useEffect } from "react";
+
+const App = ({ Component, pageProps }) => {
+
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
   return <Component {...pageProps} />
 }
+
+export default App;
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import ProgramCards from './components/ProgramCards'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -49,6 +50,11 @@ export default function Home() {
             priority
           />
         </div> */}
+
+        <ProgramCards/>
+
+
+        <button type="button" class="btn btn-secondary">Secondary</button>
 
 
       </main>
