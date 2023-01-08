4be8dfebc44f8822869985e4afee0ec2fe5d07e7
wspawned <wspawned@gmail.com>
2023-01-08 19:22:36 +0300

    Cards n fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import '../styles/globals.css'
 
+import { Roboto } from '@next/font/google'
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+})
+
 import { useEffect } from "react";
 
 const App = ({ Component, pageProps }) => {
@@ -9,7 +17,11 @@ const App = ({ Component, pageProps }) => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
 
-  return <Component {...pageProps} />
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
 
 export default App;
--- pages/components/ProgramCards/index.js
+++ pages/components/ProgramCards/index.js
@@ -1,59 +1,80 @@
 import Image from 'next/image'
 
 const ProgramCards = () => {
-  return(
-    <div class="card-deck">
-  <div class="card">
-    <Image 
-    class="card-img-top" 
-    src="/vercel.svg" 
-    alt="Card image cap"
-
-    width={100}
-    height={24}
-    priority
-    />
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  return (
+    <div class="container">
+      <div class="row card-deck">
+        <div class="col-sm-4 card text-center">
+          <div class="container p-5">
+            <Image
+              class="card-img-top"
+              src="/vercel.svg"
+              alt="Card image cap"
+              width={100}
+              height={24}
+              priority
+            />
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a longer card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+            <p class="card-text">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+        <div class="col-sm-4 card text-center">
+          <div class="container p-5">
+            <Image
+              class="card-img-top"
+              src="/vercel.svg"
+              alt="Card image cap"
+              width={100}
+              height={24}
+              priority
+            />
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This card has supporting text below as a natural lead-in to
+              additional content.
+            </p>
+            <p class="card-text">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+        <div class="col-sm-4 card text-center">
+          <div class="container p-5">
+            <Image
+              class="card-img-top"
+              src="/vercel.svg"
+              alt="Card image cap"
+              width={100}
+              height={24}
+              priority
+            />
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This card has even longer content
+              than the first to show that equal height action.
+            </p>
+            <p class="card-text">
+              <small class="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-  <div class="card">
-    <Image 
-      class="card-img-top" 
-      src="/vercel.svg" 
-      alt="Card image cap"
-
-      width={100}
-      height={24}
-      priority
-    />
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card">
-  <Image 
-      class="card-img-top" 
-      src="/vercel.svg" 
-      alt="Card image cap"
-
-      width={100}
-      height={24}
-      priority
-    />
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-</div>
-  )
-}
+  );
+};
 
 export default ProgramCards;
-
