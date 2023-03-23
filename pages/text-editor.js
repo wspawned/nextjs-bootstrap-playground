c8124291f9fa4ef0bcc49b8c5c51e78c7c2e64ce
wspawned <wspawned@gmail.com>
2023-03-24 01:50:57 +0300

    wysiwyg editor

--- pages/text-editor.js
+++ pages/text-editor.js
@@ -14,22 +14,28 @@ import draftToHtml from 'draftjs-to-html';
 const TextEditor = () => {
   const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
 
-  return(
-    <div className="flex flex-col bg-primary-300 w-screen h-screen justify-center items-center" >
-      <p className='text-3xl' >Create New Post</p>
-      <Editor
-      editorState={editorState}
-      onEditorStateChange={ () =>  setEditorState}
-      wrapperClassName="wrapper-class"
-      toolbarClassName="toolbar-class"
-      editorClassName="editor-class"
-      />
-      <textarea
-      disabled
-      value={draftToHtml(editorState.getCurrentContent())}
-      />
-    </div>
-  )
+  return (
+    <>
+      <div className="flex flex-col bg-primary-300 w-screen min-h-screen justify-star items-center pt-24 gap-8">
+        <h1 className="text-3xl">Create New Post</h1>
+        <div>
+          <Editor
+            editorState={editorState}
+            onEditorStateChange={setEditorState}
+            wrapperClassName="wrapper-class"
+            toolbarClassName="toolbar-class"
+            editorClassName="bg-white min-h-[250px] "
+          />
+          <h2 className="text-xl py-2 text-center">Preview as HTML</h2>
+          <textarea
+          className='w-full min-h-[150px] justify-center text-center '
+            disabled
+            value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default TextEditor;
